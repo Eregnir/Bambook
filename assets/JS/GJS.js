dd721e4ed8ed6dad975e3d0ecb95c318b75dae1c
Assaf Zick <zickmusic@gmail.com>
2021-04-13 18:41:35 +0300

    JS updated

--- assets/JS/GJS.js
+++ assets/JS/GJS.js
@@ -33,75 +33,75 @@ function contact()
 // }
 
 
-//---OUT OF SCOPE ALERT---
-function nom(){
-    window.alert("Action not supported in the project's scope.");
-};
+// //---OUT OF SCOPE ALERT---
+// function nom(){
+//     window.alert("Action not supported in the project's scope.");
+// };
 
-//---REVIEWS - FORM---
-//Hide the form on load
-$("#addyourown").hide();
+// //---REVIEWS - FORM---
+// //Hide the form on load
+// $("#addyourown").hide();
 
-//show the form after clicking the button
-function openForm(){
-    $("#addyourown").show();
-};
+// //show the form after clicking the button
+// function openForm(){
+//     $("#addyourown").show();
+// };
 
-//alert once submitted that the review has been added
-function alertme(){
-    alert("Thanks for adding a review! You will now be redirected to the reviews page to view it.")
-};
+// //alert once submitted that the review has been added
+// function alertme(){
+//     alert("Thanks for adding a review! You will now be redirected to the reviews page to view it.")
+// };
 
-//function to update the slider for user rating
-function updateSlider(val){
-    $("#rangeslider").html(val);
-};
+// //function to update the slider for user rating
+// function updateSlider(val){
+//     $("#rangeslider").html(val);
+// };
 
-//---CART---
-//Add to cart alert
-function alt(){
-    window.alert("Item successfully added to cart!")
-}
+// //---CART---
+// //Add to cart alert
+// function alt(){
+//     window.alert("Item successfully added to cart!")
+// }
 
-//Calculate total order cost
-function total(){
-    var A = 50.00;
-    var B = document.getElementById("q1").value;
-    var C = document.getElementById("q2").value;
-    document.getElementById("demo").innerHTML = B*A + C*A;
-}
+// //Calculate total order cost
+// function total(){
+//     var A = 50.00;
+//     var B = document.getElementById("q1").value;
+//     var C = document.getElementById("q2").value;
+//     document.getElementById("demo").innerHTML = B*A + C*A;
+// }
 
 
-//---GAME GALLERY---
-//Filter by price function
-$("#btnc1").click(function test1() {
-    if ($(this).is(":checked")) {
-        $("#r4").show();
-        $("#r3").show();
-        } else {
-            $("#r4").hide();
-            $("#r3").hide();
-        }
-    });
+// //---GAME GALLERY---
+// //Filter by price function
+// $("#btnc1").click(function test1() {
+//     if ($(this).is(":checked")) {
+//         $("#r4").show();
+//         $("#r3").show();
+//         } else {
+//             $("#r4").hide();
+//             $("#r3").hide();
+//         }
+//     });
     
-$("#btnc2").click(function test2() {
-    if ($(this).is(":checked")) {
-        $("#r2").show();
-        } else {
-            $("#r2").hide();
-        }
-    });
+// $("#btnc2").click(function test2() {
+//     if ($(this).is(":checked")) {
+//         $("#r2").show();
+//         } else {
+//             $("#r2").hide();
+//         }
+//     });
 
-$("#btnc3").click(function test3() {
-    if ($(this).is(":checked")) {
-        $("#r1").show();
-        } else {
-            $("#r1").hide();
-        }
-    });
+// $("#btnc3").click(function test3() {
+//     if ($(this).is(":checked")) {
+//         $("#r1").show();
+//         } else {
+//             $("#r1").hide();
+//         }
+//     });
 
 
-//Back to top:
-function scrollToTop(){
-    document.documentElement.scrollTop = 0;
-}
+// //Back to top:
+// function scrollToTop(){
+//     document.documentElement.scrollTop = 0;
+// }
