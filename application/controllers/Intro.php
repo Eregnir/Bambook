33d4a53b1c730e06c95295c56a3ad27203d6a232
Assaf Zick <zickmusic@gmail.com>
2021-04-14 13:31:04 +0300

    tru

--- application/controllers/Intro.php
+++ application/controllers/Intro.php
@@ -33,8 +33,8 @@ class Intro extends CI_Controller{
 
     public function new_user(){
         $data = array(
-            'fName' => $this->input->post('firstName'),
-            'lName' => $this->input->post('lastName'),
+            'f_name' => $this->input->post('firstName'),
+            'l_name' => $this->input->post('lastName'),
             'email' => $this->input->post('email'), 
             'username' => $this->input->post('username'),
             'password' => $this->input->post('password ')
