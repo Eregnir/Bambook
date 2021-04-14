9252554c7ae1a9119f48db03f5833336cb13ea9b
Assaf Zick <zickmusic@gmail.com>
2021-04-14 11:32:32 +0300

    trying to get the small navbar to work

--- application/views/B_Views/index.php
+++ application/views/B_Views/index.php
@@ -43,7 +43,7 @@
         <div class="mdl-layout__drawer mdl-layout--small-screen-only">
             <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
                     <a id="ind" class="mdl-navigation__link is-active" href="">Portfolio</a>
-                    <a id="blg" class="mdl-navigation__link" href="">Blog</a>
+                    <a id="blg2" class="mdl-navigation__link" href="">Blog</a>
                     <a id="abt" class="mdl-navigation__link" href="">About</a>
                     <a id="cnt" class="mdl-navigation__link" href="">Contact</a>
             </nav>
@@ -199,6 +199,11 @@
     };
 
     document.getElementById("blg").onclick=function()
+    {
+        window.location.href="<?php echo site_url('Intro/blog');?>";   
+    };
+
+    document.getElementById("blg2").onclick=function()
     {
         window.location.href="<?php echo site_url('Intro/blog');?>";   
     };
