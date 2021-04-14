234380f38a7c66404e0413584c6d45250965c8e9
Assaf Zick <zickmusic@gmail.com>
2021-04-14 12:38:43 +0300

    register form structure

--- application/views/B_Views/register.php
+++ application/views/B_Views/register.php
@@ -45,32 +45,45 @@
             <div class="mdl-grid portfolio-max-width portfolio-contact">
                 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
                     <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">Contact</h2>
+                        <h2 class="mdl-card__title-text">Register to Bambook</h2>
                     </div>
                     <div class="mdl-card__media">
                         <img class="article-image" src="<?php echo base_url('images/contact-image.jpg');?>" border="0" alt="">
                     </div>
                     <div class="mdl-card__supporting-text">
                         <p>
-                            Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
+                            This text will welcome the new user to Bambook
                         </p>
                         <p>
-                            Excepteur reprehenderit sint exercitation ipsum consequat qui sit id velit elit. Velit anim eiusmod labore sit amet.
+                            And this text as well.
                         </p>
+                        <!-- Registration Form -->
                         <form action="#" class="">
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" pattern="[A-Z,a-z, ]*" type="text" id="Name">
-                                <label class="mdl-textfield__label" for="Name">Name...</label>
+                                <label class="mdl-textfield__label" for="firstName">First Name...</label>
                                 <span class="mdl-textfield__error">Letters and spaces only</span>
+                            </div>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" pattern="[A-Z,a-z, ]*" type="text" id="Name">
+                                <label class="mdl-textfield__label" for="lastName">Last Name...</label>
+                                <span class="mdl-textfield__error">Letters and spaces only</span>
+                            </div>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{2,15}$" type="text" id="Name">
+                                <label class="mdl-textfield__label" for="username">Username...</label>
+                                <span class="mdl-textfield__error">Username must contain 3-20 characters</span>
                             </div>
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                 <input class="mdl-textfield__input" type="text" id="Email">
                                 <label class="mdl-textfield__label" for="Email">Email...</label>
                             </div>
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                <textarea class="mdl-textfield__input" type="text" rows="5" id="note"></textarea>
-                                <label class="mdl-textfield__label" for="note">Enter note</label>
+                                <input class="mdl-textfield__input" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" type="text" id="Name">
+                                <label class="mdl-textfield__label" for="username">Username...</label>
+                                <span class="mdl-textfield__error">Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters</span>
                             </div>
+                            <!-- Need to add password validation -->
                             <p>
                                 <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
                                     Submit
