8f23eb5cf728c5f00c55ec99da4253100e12789e
Assaf Zick <zickmusic@gmail.com>
2021-04-14 19:53:41 +0300

    change footer background

--- application/views/templates/FootB.php
+++ application/views/templates/FootB.php
@@ -1,10 +1,10 @@
             <footer class="mdl-mini-footer">
                 <div class="mdl-mini-footer__left-section">
-                    <div class="mdl-logo">Simple portfolio website</div>
+                    <div class="mdl-logo">Bambook - Book Swap</div>
                 </div>
                 <div class="mdl-mini-footer__right-section">
                     <ul class="mdl-mini-footer__link-list">
-                        <li><a href="#">Help</a></li>
+                        <li><a href="#">An MTA - Information Systems Project</a></li>
                         <li><a href="#">Privacy & Terms</a></li>
                     </ul>
                 </div>
