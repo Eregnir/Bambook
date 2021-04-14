6a69d417fc8191a4fb02498660bcc8d34d4c9092
Assaf Zick <zickmusic@gmail.com>
2021-04-14 11:51:49 +0300

    fixed small navbar issues

--- application/views/B_Views/contact.php
+++ application/views/B_Views/contact.php
@@ -22,7 +22,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="A portfolio template that uses Material Design Lite.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <title>MDL-Static Website</title>
+    <title>Bambook</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-pink.min.css" />
     <link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css');?> " />
@@ -35,7 +35,7 @@
             <div class="mdl-layout__header-row portfolio-logo-row">
                 <span class="mdl-layout__title">
                     <div class="portfolio-logo"></div>
-                <span class="mdl-layout__title">Simple portfolio website</span>
+                <span class="mdl-layout__title">Bambook - Book Swap</span>
                 </span>
             </div>
             <div class="mdl-layout__header-row portfolio-navigation-row mdl-layout--large-screen-only">
@@ -49,10 +49,10 @@
         </header>
         <div class="mdl-layout__drawer mdl-layout--small-screen-only">
             <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
-                <a id="ind" class="mdl-navigation__link" href="#">Portfolio</a>
-                <a id="blg" class="mdl-navigation__link" href="#">Blog</a>
-                <a id="abt" class="mdl-navigation__link" href="#">About</a>
-                <a id="cnt" class="mdl-navigation__link is-active" href="">Contact</a>
+                <a class="mdl-navigation__link" href="<?php echo site_url('Intro/index');?>">Homepage</a>
+                <a class="mdl-navigation__link" href="<?php echo site_url('Intro/blog');?>">Blog</a>
+                <a class="mdl-navigation__link" href="<?php echo site_url('Intro/about');?>">About</a>
+                <a class="mdl-navigation__link is-active" href="<?php echo site_url('Intro/contact');?>">Contact</a>
             </nav>
         </div>
         <main class="mdl-layout__content">
