40c8eee288e914b71293edb564d83fcf80f0ff85
Assaf Zick <zickmusic@gmail.com>
2021-04-15 13:31:41 +0300

    both lines of username should work now

--- application/controllers/Intro.php
+++ application/controllers/Intro.php
@@ -14,6 +14,7 @@ class Intro extends CI_Controller{
         if ($user['loggedin']!=null){
              
             $data['test']=$user['username'];
+            $data['user']=$user;
         }
         $data['email']=$this->intro_model->test();
         $this->load->view('templates/HeadB',$data);
