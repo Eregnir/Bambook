dcb5aa8a2b711458cf5a06b5602c5096163ba31f
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-25 18:14:43 +0300

    changestable

--- application/views/templates/HeadB.php
+++ application/views/templates/HeadB.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="<?php echo base_url('/assets/css/styles.css');?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="icon" href="../images/favicon.png" type="image/png">  
+    <link rel="icon" href="images/favicon.png" type="image/png">  
 </head>
 
 <body>
@@ -46,6 +46,6 @@
                     <a class="mdl-navigation__link" href="<?php echo site_url('Intro/login');?>">Login</a>
             </nav>
         </div>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
