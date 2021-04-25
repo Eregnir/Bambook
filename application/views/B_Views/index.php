214035dbd68755e00b80d91a2759dfe68c6a34fe
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-25 13:27:29 +0300

    changed index page

--- application/views/B_Views/index.php
+++ application/views/B_Views/index.php
@@ -4,13 +4,15 @@
                 <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
                     <div class="mdl-card__media">
                         <a href="portfolio-example01.html">
-                            <img class="article-image" src="<?php echo base_url('images/example-work01.jpg');?>" border="0" alt="">
+                            <img class="article-image" src="<?php echo base_url('images/available_books.jpg');?>" border="0" alt="">
                         </a>
                     </div>
                     <div class="mdl-card__title">
                         <h2 class="mdl-card__title-text">Available Books</h2>
                     </div>
                     <div class="mdl-card__supporting-text">
+                        Bambook's available books can be found here.<br>
+                        Start searching for a new book to read from our growing community!
                         <!-- Text here if needed -->
                     </div>
                     <div class="mdl-card__actions mdl-card--border">
@@ -22,7 +24,7 @@
                 <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
                     <div class="mdl-card__media">
                         <a href="portfolio-example02.html">
-                            <img class="article-image" src="<?php echo base_url('images/available_books.jpg');?>" border="0" alt="available_books">
+                            <img class="article-image" src="<?php echo base_url('images/my_requests.jpg');?>" border="0" alt="available_books">
                         </a>
                     </div>
                     <div class="mdl-card__title">
@@ -30,6 +32,7 @@
                         <h2 class="mdl-card__title-text">My Requests</h2>
                     </div>
                     <div class="mdl-card__supporting-text">
+                        Here you can find all of your message requests and communicate with other users.
                          <!-- Text here if needed -->
                     </div>
                     <div class="mdl-card__actions mdl-card--border">
@@ -41,13 +44,15 @@
                 <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
                     <div class="mdl-card__media">
                         <a href="portfolio-example03.html">
-                            <img class="article-image" src="<?php echo base_url('images/my_requests.jpg');?>" border="0" alt="my_requests">
+                            <img class="article-image" src="<?php echo base_url('images/add_books.jpg');?>" border="0" alt="my_requests">
                         </a>
                     </div>
                     <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">Add books</h2>
+                        <h2 class="mdl-card__title-text">List a book</h2>
                     </div>
                     <div class="mdl-card__supporting-text">
+                        Finished reading a book? Your old books are getting dusty?<br>
+                        List a book you want to offer for a swap and let others enjoy it.
                          <!-- Text here if needed -->
                     </div>
                     <div class="mdl-card__actions mdl-card--border">
