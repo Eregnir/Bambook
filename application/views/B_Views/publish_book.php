a90b4de1a517036da00582d2e9bb2479b180280f
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-25 22:28:39 +0300

    changes publish page

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -1,5 +1,4 @@
         <main class="mdl-layout__content">
-            <div class="mdl-grid portfolio-max-width">
                 <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
                 <!-- Form publish a book -->
                     <div class="container">
@@ -119,7 +118,6 @@
                             </div>
                         </div>  
                     </div>
-                </div>
             </div>
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/JS/GJS.js');?>"></script>
