b2acbf9f38a291a61f40bd2d70344e1d1a8f0404
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-25 22:26:27 +0300

    changes publish book

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -4,78 +4,123 @@
                 <!-- Form publish a book -->
                     <div class="container">
                         <div class="row">
-                            <div class="col-8">
-                            <form class="needs-validation" novalidate>
-                                <div class="form-group">
-                                <label for="name">Name*</label>
-                                <input type="text" class="form-control" id="name" placeholder="Your name" required>
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                                <div class="invalid-feedback">
-                                    Please add a name.
-                                </div>
-                                </div>
-
-                                <div class="form-group">
-                                <label for="telephone">Telephone*</label>
-                                <input type="text" class="form-control" id="telephone" placeholder="Your telephone number" required minlength="7" maxlength="12">
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                                <div class="invalid-feedback">
-                                    Please choose a valid telephone number (between 7 and 10 characters).
-                                </div>
-                                </div>
-
-                                <div class="form-group">
-                                <label for="address">Address</label>
-                                <input type="text" class="form-control" id="address" placeholder="Your delivery address" required>
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                                <div class="invalid-feedback">
-                                    Please add an address.
-                                </div>
-                                </div>
-
-                                <div class="form-group">
-                                <select class="custom-select" required>
-                                    <option value="">Select a menu*</option>
-                                    <option value="1">Breakfast</option>
-                                    <option value="2">Lunch</option>
-                                    <option value="3">Dinner</option>
-                                </select>
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                                <div class="invalid-feedback">
-                                    Please choose a menu.
-                                </div>
-                                </div>
-
-                                <div class="form-group">
-                                <label>I need cutlery:</label>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="yes" name="cutlery" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="yes">Yes</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="no" name="cutlery" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="no">No</label>
-                                    <div class="valid-feedback">
-                                        Looks good!
+                            <div class="offset-lg-2 col-lg-8 col-sm-8 col-8 border rounded main-section">
+                                <h3 class="text-center text-inverse">Form Validation</h3>
+                                <hr>
+                                <form class="container" id="needs-validation" novalidate>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="validationCustom01">First Name</label>
+                                        <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="First Name" required>
                                     </div>
-                                    <div class="invalid-feedback">
-                                        Please specify if you need cutlery.
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="validationCustom02">Last Name</label>
+                                        <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Last Name" required>
+                                    </div>  
                                     </div>
                                 </div>
+                                <div class="row">
+                                    <div class="col-md-6 col-sm-12 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="inputEmail4">Email</label>
+                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" required>
+                                        <div class="invalid-feedback">
+                                        Please provide a valid Email.
+                                        </div>
+                                    </div>  
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="inputpassword">Password</label>
+                                        <input type="password" class="form-control" id="inputpassword" placeholder="password" required>
+                                        <div class="invalid-feedback">
+                                        Please provide a valid password.
+                                        </div>
+                                    </div>  
+                                    </div>
                                 </div>
-                                <button type="submit" class="btn btn-info">Submit</button>
-                            </form>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="validationCustom03">City</label>
+                                        <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+                                        <div class="invalid-feedback">
+                                        Please provide a valid city.
+                                        </div>
+                                    </div>  
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="text-inverse" for="select-menu">Select any value</label>
+                                        <select class="custom-select d-block form-control" id="image" required>
+                                        <option value="">Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                        </select>
+                                        <div class="invalid-feedback">
+                                        Please selected any option.
+                                        </div>
+                                    </div>  
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="custom-file">
+                                        <input type="file" id="file" class="form-control custom-file-input" required>
+                                        <span class="custom-file-control"></span>
+                                        <div class="invalid-feedback">
+                                            Please selected any File.
+                                        </div>
+                                        </label>
+                                    </div>  
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" required>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Check this custom checkbox</span>
+                                        </label>
+                                    </div>  
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="custom-control custom-radio">
+                                        <input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input" required>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Toggle this custom radio</span>
+                                        </label>
+                                    </div>  
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label class="custom-control custom-radio">
+                                        <input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input" required>
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Or toggle this other custom radio</span>
+                                        </label>
+                                    </div>  
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-lg-12 col-sm-12 col-12 text-center">
+                                        <button class="btn btn-info" type="submit">Submit form</button>
+                                    </div>
+                                </div>  
+                                </form>
                             </div>
-                        </div>
+                        </div>  
                     </div>
+                </div>
+            </div>
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/JS/GJS.js');?>"></script>
 
@@ -86,16 +131,12 @@
         }
     </script>
 
-    <!-- Validation for errors -->
+    <!-- Form Validation -->
     <script>
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
+          (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+            var form = document.getElementById('needs-validation');
             form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
                 event.preventDefault();
@@ -103,7 +144,6 @@
                 }
                 form.classList.add('was-validated');
             }, false);
-            });
-        }, false);
+            }, false);
         })();
     </script>
