bcaa7f5756e5b7867ee26727e2ef9450ba51c57f
Assaf Zick <zickmusic@gmail.com>
2021-04-26 12:29:32 +0300

    abt chng

--- application/views/B_Views/about.php
+++ application/views/B_Views/about.php
@@ -13,8 +13,8 @@
                         <u><h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline">My Profile:</h3></u>
                         <div class="mdl-cell mdl-cell--8-col mdl-card__supporting-text no-padding ">
                             <p>
-                            <h4><br>Hello, <span> <?php foreach ($profile as $prof){echo $prof->username;}?>! </span></h4><br>
-                            <?php foreach ($profile as $prof){echo '<img style="max-height:250px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $prof->avatar).'"/>';}?>   
+                            <h4><br>Hello, <?php foreach ($profile as $prof){echo $prof->username;}?>! </h4><br>
+                            <?php foreach ($profile as $prof){echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $prof->avatar).'"/>';}?>   
                                     <h5>
                                         <br><br><b>Name:</b> <?php foreach ($profile as $prof){echo $prof->f_name." ". $prof->l_name;}?>
                                         <br><br><b>Email:</b> <?php foreach ($profile as $prof){echo $prof->email;}?>
