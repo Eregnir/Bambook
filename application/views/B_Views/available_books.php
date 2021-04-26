6c4864bd9281d904d6bfaeeb3dcbc317c1f3bb3a
Assaf Zick <zickmusic@gmail.com>
2021-04-26 13:58:14 +0300

    images are now O.K

--- application/views/B_Views/available_books.php
+++ application/views/B_Views/available_books.php
@@ -4,11 +4,6 @@
                     <h2 class="pad5">Available Books</h2>
                     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for the book or author name..." title="Type in a name">
                     <br>
-                    <?php
-                    foreach($books as $book){
-                        echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $book->img).'"/>';
-                    }
-                    ?>
                     <table class="table table-image" id="myTable">
                         <thead>
                             <tr>
@@ -20,11 +15,10 @@
                         </thead>
                         
                         <tbody>
-                            <!-- I can't get the table to show the images of the book for some reason. don't know why. -->
                         <?php foreach ($books as $book):?>
                                 <tr id="<?php echo $book->UID?>" class="table-row">
                                     <td class="w-25">
-                                        <span class="img-fluid img-thumbnail"> <?php echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $book->img).'"/>';?> </span>
+                                        <span class="img-fluid"> <?php echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $book->img).'"/>';?> </span>
                                     </td>
                                     <td> <?php echo $book->title ?> </td>
                                     <td> <?php echo $book->author ?> </td>
