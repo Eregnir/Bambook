2db4cd7b9935928a5c0a61f6f8ca62cc590781c2
Assaf Zick <zickmusic@gmail.com>
2021-04-27 19:06:50 +0300

    form close

--- application/views/B_Views/available_books.php
+++ application/views/B_Views/available_books.php
@@ -23,13 +23,14 @@
                                     <td class="w-25">
                                         <?php echo form_open('Books/single_book', $attributes); ?>
                                         <input type="hidden" value="<?php echo $book->UID;?>" name="b_UID" id="<?php echo 'book_'.$book->UID?>">
-                                        <?php echo form_close(); ?>
                                         <span class="img-fluid"> <?php echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $book->img).'"/>';?> <br></span>
                                         <button class="mdl-button mdl-js-button mdl-button--icon" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
+                                        <?php echo form_close(); ?>
                                     </td>
                                     <td> <?php echo $book->title ?> </td>
                                     <td> <?php echo $book->author ?> </td>
                                     <td> <?php echo $book->cond ?> </td>
+                            
                                 </tr>
                         <?php endforeach; ?>
                         </tbody>
