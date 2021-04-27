35c10721dd0990557f93b9593bd965a1372575f2
Assaf Zick <zickmusic@gmail.com>
2021-04-27 20:37:13 +0300

    most of the book details are now from DB

--- application/views/B_Views/test.php
+++ application/views/B_Views/test.php
@@ -1,18 +1,18 @@
         <main class="mdl-layout__content">
             <div class="mdl-grid portfolio-max-width">
                 <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
-                    <?php
-                        echo $b_UID;
-                        foreach ($book_info as $bi):?>
+                    <?php foreach ($book_info as $bi):?>
 
                     <div class="card" style="width: 25rem">
+                    <!-- book image -->
                         <span class="card-img-top"><?php echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $bi->img).'"/>';?></span>
                         <div class="card-body">
-                            <h2 class="card-title">The Lovely Bones
-                                    ; ?> </h2> <!--Book title-->
-                            <h4 class="card-title">Alice Sebold</h4> <!--Book Author-->
-                            <h5 class="card-text">3.82</h5> <!--Rating-->
-                            <p class="card-text"><!-- Book description -->
+                            <!-- book title -->
+                            <h2 class="card-title"><?php echo $bi->title ?></h2> 
+                            <!--Book Author-->
+                            <h4 class="card-title"><?php echo $bi->author ?></h4> 
+                            <!-- Book description -->
+                            <p class="card-text"><
                                 "My name was Salmon, like the fish; first name, Susie. I was fourteen when I was murdered on December 6, 1973."
                                 So begins the story of Susie Salmon, who is adjusting to her new home in heaven, a place that is not at all what she expected,
                                 even as she is watching life on earth continue without her -- her friends trading rumors about her disappearance,
@@ -22,8 +22,10 @@
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">Genre: Fiction</li><!-- to add these details from the DB -->
-                            <li class="list-group-item">Language: English</li>
-                            <li class="list-group-item">Condition: Like new</li>
+                            <!-- Book Language -->
+                            <li class="list-group-item"><?php echo $bi->lang?></li>
+                            <!-- Book Condition -->
+                            <li class="list-group-item"><?php echo $bi->cond ?></li>
                         </ul>
                         <div class="card-body">
                             <a href="javascript:history.back()" class="card-link alignleft">Go Back</a>
