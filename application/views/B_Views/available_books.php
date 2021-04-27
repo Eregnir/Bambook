05237bb9678f3ce01c2ade483559de80da91377b
Assaf Zick <zickmusic@gmail.com>
2021-04-27 19:05:02 +0300

    show book doesnt work?

--- application/views/B_Views/available_books.php
+++ application/views/B_Views/available_books.php
@@ -17,19 +17,19 @@
                         <tbody>
                         <?php foreach ($books as $book):
                         //Open a form that will send the book UID to the controller in order to show it's full details.
-                            $attributes = array('id' => $book->UID, 'name' =>$book->UID);
-                            echo form_open('Books/single_book', $attributes); ?>
-                            <input type="hidden" value="<?php echo $book->UID;?>" name="b_UID" id="<?php echo 'book_'.$book->UID?>"> 
+                            $attributes = array('id' => $book->UID, 'name' =>$book->UID);?>
                             
                                 <tr id="<?php echo $book->UID?>" class="table-row">
                                     <td class="w-25">
+                                        <?php echo form_open('Books/single_book', $attributes); ?>
+                                        <input type="hidden" value="<?php echo $book->UID;?>" name="b_UID" id="<?php echo 'book_'.$book->UID?>">
+                                        <?php echo form_close(); ?>
                                         <span class="img-fluid"> <?php echo '<img style="max-height:200px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $book->img).'"/>';?> <br></span>
                                         <button class="mdl-button mdl-js-button mdl-button--icon" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
                                     </td>
                                     <td> <?php echo $book->title ?> </td>
                                     <td> <?php echo $book->author ?> </td>
                                     <td> <?php echo $book->cond ?> </td>
-                            <?php echo form_close(); ?>
                                 </tr>
                         <?php endforeach; ?>
                         </tbody>
