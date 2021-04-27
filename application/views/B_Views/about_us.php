19709d110ff0b26d509e666e7feb315cf1389823
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-27 22:57:33 +0300

    about page changes2

--- application/views/B_Views/about_us.php
+++ application/views/B_Views/about_us.php
@@ -9,24 +9,22 @@
                         <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
                             If a match is found, both users will be notified so they can arrange the book swap details.</p>
                     </div>
-                    <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
-                        <!-- Image card 1 -->
-                        <div class="mdl-card">
-                        <div class="mdl-card__title">
-                            <h2 class="mdl-card__title-text">Auckland Sky Tower<br>Auckland, New Zealand</h2>
-                        </div>
-                        <div class="mdl-card__media">
-                            <img src="..assets/images/about_us_images/1.png" width="400" height="400" border="0" alt=""
-                            style="padding:10px;">
-                        </div>
-                        <div class="mdl-card__supporting-text">
-                        The Sky Tower is an observation and telecommunications tower located in Auckland,
-                        New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
-                        in the Southern Hemisphere.
-                        </div>
-                        <div class="mdl-card__actions">
-                            <a href="http://en.wikipedia.org/wiki/Sky_Tower_%28Auckland%29">Wikipedia entry</a>
-                        </div>
+                    <!-- Image card 1 -->
+                    <div class="mdl-card">
+                    <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">Auckland Sky Tower<br>Auckland, New Zealand</h2>
+                    </div>
+                    <div class="mdl-card__media">
+                        <img src="..assets/images/about_us_images/1.png" width="400" height="400" border="0" alt=""
+                        style="padding:10px;">
+                    </div>
+                    <div class="mdl-card__supporting-text">
+                    The Sky Tower is an observation and telecommunications tower located in Auckland,
+                    New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
+                    in the Southern Hemisphere.
+                    </div>
+                    <div class="mdl-card__actions">
+                        <a href="http://en.wikipedia.org/wiki/Sky_Tower_%28Auckland%29">Wikipedia entry</a>
                     </div>
                 </div>
             </div>
