f05748ff2aadb2d7ddaab335f87373782abf8b1c
Assaf Zick <zickmusic@gmail.com>
2021-04-27 20:32:52 +0300

    now

--- application/views/B_Views/test.php
+++ application/views/B_Views/test.php
@@ -36,6 +36,7 @@
                     </div> 
                 </div>
             </div>
+            <?php endforeach; ?>
 
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/JS/GJS.js');?>"></script>
