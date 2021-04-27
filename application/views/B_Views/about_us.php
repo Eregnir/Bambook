2ae56f34289f60cfc4f5df3102fa7c16ce04d9bb
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-27 22:50:45 +0300

    change about page

--- application/views/B_Views/about_us.php
+++ application/views/B_Views/about_us.php
@@ -1,31 +1,32 @@
         <main class="mdl-layout__content">
             <div class="mdl-grid portfolio-max-width">
-                <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
-                    <div class="mdl-grid portfolio-copy">
-                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>About us:</u></h3>
-                        <p>Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
-                            With Bambook you can swap the books you no longer need with books you would like to read.</p>
-                        <p>After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.</p>
-                        <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
-                             If a match is found, both users will be notified so they can arrange the book swap details.</p>
-                    </div>
-                    <!-- Image card 1 -->
-                    <div class="mdl-card">
-                    <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">Auckland Sky Tower<br>Auckland, New Zealand</h2>
-                    </div>
-                    <div class="mdl-card__media">
-                        <img src="..assets/images/about_us_images/1.png" width="400" height="400" border="0" alt=""
-                        style="padding:10px;">
-                    </div>
-                    <div class="mdl-card__supporting-text">
-                    The Sky Tower is an observation and telecommunications tower located in Auckland,
-                    New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
-                    in the Southern Hemisphere.
-                    </div>
-                    <div class="mdl-card__actions">
-                        <a href="http://en.wikipedia.org/wiki/Sky_Tower_%28Auckland%29">Wikipedia entry</a>
-                    </div>
+                <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
+                    <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+                        <div class="mdl-grid portfolio-copy">
+                            <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>About us:</u></h3>
+                            <p>Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
+                                With Bambook you can swap the books you no longer need with books you would like to read.</p>
+                            <p>After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.</p>
+                            <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
+                                If a match is found, both users will be notified so they can arrange the book swap details.</p>
+                        </div>
+                        <!-- Image card 1 -->
+                        <div class="mdl-card">
+                        <div class="mdl-card__title">
+                            <h2 class="mdl-card__title-text">Auckland Sky Tower<br>Auckland, New Zealand</h2>
+                        </div>
+                        <div class="mdl-card__media">
+                            <img src="..assets/images/about_us_images/1.png" width="400" height="400" border="0" alt=""
+                            style="padding:10px;">
+                        </div>
+                        <div class="mdl-card__supporting-text">
+                        The Sky Tower is an observation and telecommunications tower located in Auckland,
+                        New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
+                        in the Southern Hemisphere.
+                        </div>
+                        <div class="mdl-card__actions">
+                            <a href="http://en.wikipedia.org/wiki/Sky_Tower_%28Auckland%29">Wikipedia entry</a>
+                        </div>
                     </div>
                 </div>
             </div>
