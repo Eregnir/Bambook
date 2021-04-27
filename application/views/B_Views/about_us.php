3e031d4490ad2f39bcc305de21b22f9a0baaa3db
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-04-27 22:54:30 +0300

    changes about page

--- application/views/B_Views/about_us.php
+++ application/views/B_Views/about_us.php
@@ -1,15 +1,15 @@
         <main class="mdl-layout__content">
             <div class="mdl-grid portfolio-max-width">
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
-                    <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
-                        <div class="mdl-grid portfolio-copy">
-                            <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>About us:</u></h3>
-                            <p>Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
-                                With Bambook you can swap the books you no longer need with books you would like to read.</p>
-                            <p>After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.</p>
-                            <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
-                                If a match is found, both users will be notified so they can arrange the book swap details.</p>
-                        </div>
+                <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+                    <div class="mdl-grid portfolio-copy">
+                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>About us:</u></h3>
+                        <p>Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
+                            With Bambook you can swap the books you no longer need with books you would like to read.</p>
+                        <p>After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.</p>
+                        <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
+                            If a match is found, both users will be notified so they can arrange the book swap details.</p>
+                    </div>
+                    <div class="mdl-cell mdl-cell--4-col mdl-cell--4-offset"> <!-- centered div -->
                         <!-- Image card 1 -->
                         <div class="mdl-card">
                         <div class="mdl-card__title">
