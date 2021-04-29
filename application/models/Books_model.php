d7821c368ce59aeebf4a3dbe18ddd226ce1c73ad
Assaf Zick <zickmusic@gmail.com>
2021-04-29 17:55:17 +0300

    sql changes

--- application/models/Books_model.php
+++ application/models/Books_model.php
@@ -14,7 +14,7 @@ class books_model extends CI_Model {
 
     public function send_swap_req($data){
         $sent_to_username=$this->db->query('SELECT username FROM users WHERE email= "'.$data['email'].'" ');
-        $query=$this->db->query('SELECT date_registered FROM users WHERE username= "'.$sent_to_username.'" ');
+        $query=$this->db->query('SELECT date_registered FROM users WHERE username= "'.$sent_to_username['username'].'" ');
         return $query->result();
     }
     
