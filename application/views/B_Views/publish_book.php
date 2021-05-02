8cae15989b3ead0e96bbe38093a3b0fb6b377660
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-02 14:26:08 +0300

    changes style

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -4,7 +4,7 @@
 			<div class="row">
 				<div class="col-md-7 m-x-auto pull-xs-none offset-3"> <!--offset-3 to center the div-->
 					<div class="jumbotron">
-						<h2 class="h2-responsive"><strong>Publish a book</strong></h2>
+						<h2 class="h2-responsive"><strong>Publish a book</strong></h2><br>
 						<!-- <hr class="m-y-2"> -->
 
 						<!--Naked Form-->
@@ -14,7 +14,7 @@
 							<form action="#">
                                 <!-- Book Genre -->
                                 <div class="form-group">
-                                <label class="control-label" for="book_genre">Genres*</label><i class="bar"></i>
+                                <label class="control-label" for="book_genre">Genres*</label><i class="bar"></i><br>
                                 <select name="genre" id="book_genre" name="book_genre" required="required">
                                     <option value="fantasy">Fantasy</option>
                                     <option value="mystery">Mystery</option>
@@ -27,21 +27,21 @@
                                 </div>
                                 <!-- Book Title -->
                                 <div class="form-group">
-                                <label class="control-label" for="book_title">Book Title*</label>
+                                <label class="control-label" for="book_title">Book Title*</label><br>
                                 <input type="text" id="book_title" name="book_title" required="required" placeholder="Enter your book title"/>
                                         <i class="bar"></i>
                                         <i class="input-error">Please enter a book name</i>
                                 </div>
                                 <!-- Book Author -->
                                 <div class="form-group">
-                                <label class="control-label" for="book_author">Author*</label>
+                                <label class="control-label" for="book_author">Author*</label><br>
                                 <input type="text" id="book_author" name="book_author" required="required" placeholder="Enter book author"/>
                                         <i class="bar"></i>
                                         <i class="input-error">Please enter book author</i>			 
                                 </div>
                                 <!-- Book condition -->
                                 <div class="form-group">
-                                <label class="control-label" for="book_cond">Condition*</label><i class="bar"></i>
+                                <label class="control-label" for="book_cond">Condition*</label><i class="bar"></i><br>
                                 <select name="book_cond" id="book_cond" required="required">
                                     <option value="new">New</option>
                                     <option value="like_new">Like New</option>
@@ -49,7 +49,7 @@
                                 </select>
                                 </div>
                                 <!-- Submit a button -->
-                                <div class="text-xs-center">
+                                <div class="text-xs">
                                     <button class="btn btn-primary">Publish a book</button>
                                 </div>
 							</form>
