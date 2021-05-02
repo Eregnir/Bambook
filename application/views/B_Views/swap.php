3500d3f146f3e94ceeda92bd563bec6601e5b39d
Assaf Zick <zickmusic@gmail.com>
2021-05-02 19:34:19 +0300

    6

--- application/views/B_Views/swap.php
+++ application/views/B_Views/swap.php
@@ -10,7 +10,7 @@
 
                 <!-- If the status is completed, show contact phone! -->
                 <span> 
-                <a href="https://api.whatsapp.com/send?phone=+972503332850/?text=hello">
+                <a href="https://api.whatsapp.com/send?phone=+972503332860/?text=hello">
                 <img style="height:50px; width:50px;" class="card-img-top" alt="Contact" src='<?php echo base_url('images/whatsapp.png');?>' >
                 </a>
                 </span>
