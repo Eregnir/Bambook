53fb0afe18c15f765b13883bbdf9e8f4bb7c0694
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-02 19:13:20 +0300

    changes

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -121,7 +121,7 @@
                         
                         // Published Date
                         if (typeof json.items[i].volumeInfo.publishedDate != "undefined") {
-                        p_date = json.items[i].volumeInfo.publishedDate[0];
+                        p_date = json.items[i].volumeInfo.publishedDate;
                         }
 
                         if (typeof json.items[i].volumeInfo.industryIdentifiers != "undefined") {
