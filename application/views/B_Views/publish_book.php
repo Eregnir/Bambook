bb0e37a903824681b85687081ede8e3391ac6b04
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-02 12:46:18 +0300

    changes publish book

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -2,6 +2,8 @@
             <div class="mdl-grid portfolio-max-width">
                 <h2 class="pad5">Publish a book</h2>
                     
+            </div>
+        </main>
 
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/JS/GJS.js');?>"></script>
