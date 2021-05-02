f3a17c832e8b6285bbdf9557a16fb02f3ae2d246
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-02 17:58:02 +0300

    changes upload book

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -10,10 +10,20 @@
                                 <div class="card-block">
                                     <!--Body-->
                                     <form action="#">
+                                        <!-- Search Book using Google API -->
+                                        <h5 class="h5-responsive">Search your book name & save time on the upload</h5>
+                                        <div id="search">
+                                            <form id="search_form">
+                                                <input type="text" size="70" id="search_txt" placeholder="Book title, author or ISBN"/>
+                                                <input type="submit" value="Search Books">
+                                            </form>
+                                            </div>
+                                            <br>
+                                        <div id="books"></div>
                                         <!-- Book Genre -->
                                         <div class="form-group">
-                                            <label for="book_genre" class="bmd-label-floating">Genre*</label>
-                                            <select class="form-control" name="book_genre" id="book_genre" name="book_genre" required="required">
+                                            <label for="book_genre" class="bmd-label-floating">Genre</label>
+                                            <select class="form-control" name="book_genre" id="book_genre" name="book_genre">
                                                 <option value="fantasy">Fantasy</option>
                                                 <option value="mystery">Mystery</option>
                                                 <option value="romance">Romance</option>
@@ -74,3 +84,83 @@
 
         <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url('assets/JS/GJS.js');?>"></script>
+
+        <!-- JS for Google API search -->
+
+        <script>
+            $("#search_form").submit(function(e) {
+                $("#books").html("");
+
+                e.preventDefault();
+
+                var searchQuery = $("#search_txt").val();
+                searchQuery = searchQuery.split(" ").join("+");
+
+                if (!searchQuery) {
+                    searchQuery = "paleo";
+                }
+
+                $.ajax({
+                    url: "https://www.googleapis.com/books/v1/volumes?q=" + searchQuery,
+                    success: function(json) {
+                    var htmlcontent = "";
+                    var thumb = "";
+                    var author = "";
+                    var description = "";
+                    var isbn = "";
+
+                    for (i = 0; i < json.items.length; i++) {
+                        if (typeof json.items[i].volumeInfo.imageLinks != "undefined") {
+                        thumb = json.items[i].volumeInfo.imageLinks.smallThumbnail;
+                        } else {
+                        thumb = "http://i.imgur.com/oM3MdAi.png"; // image not available
+                        //thumb = 'http://slems-oldboys.org.uk/library/wp-content/uploads/2013/11/library_nocover.jpg'
+                        }
+                        // AUTHOR
+                        if (typeof json.items[i].volumeInfo.authors != "undefined") {
+                        author = json.items[i].volumeInfo.authors[0];
+                        }
+                        
+                            // AUTHOR
+                        if (typeof json.items[i].volumeInfo.description != "undefined") {
+                        description = json.items[i].volumeInfo.description;
+                        }
+                        
+                        if (typeof json.items[i].volumeInfo.industryIdentifiers != "undefined") {
+                        isbn = json.items[i].volumeInfo.industryIdentifiers[0].identifier;
+                        }
+
+                        htmlcontent +=
+                        "<div class='thumbs'><b>Title:</b> " +
+                        json.items[i].volumeInfo.title +
+                        "</b> " +
+                        '<img src="' +
+                        thumb +
+                        '" + alt="' +
+                        json.items[i].volumeInfo.title +
+                        '">' +
+                        "<br><b>Author: </b>" +
+                        author +
+                        "<br><b>ISBN_13: </b>" +
+                        isbn +
+                        "<br><br>" +
+                        "<b>description:</b> " +
+                        trunc(description, 400) +
+                        "</div>";
+                    }
+                    document.getElementById("books").innerHTML =
+                        "<div>" + htmlcontent + "</div><br>";
+                    }
+                });
+            });
+
+            function trunc(s, n) {
+            //if(typeof s !== "undefined"){
+            var t = s.indexOf(" ", n);
+            if (t == -1) return s;
+            return s.substring(0, t) + "...";
+            // } else {
+            //   return s;
+            // }
+            }
+        </script>
