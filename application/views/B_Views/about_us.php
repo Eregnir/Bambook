cd6e01eb3c756edc30e5df61e83299764cb3162b
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-09 15:33:22 +0300

    changes index

--- application/views/B_Views/about_us.php
+++ application/views/B_Views/about_us.php
@@ -2,66 +2,73 @@
             <div class="mdl-grid portfolio-max-width">
                 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
                     <div class="mdl-grid portfolio-copy">
-                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>About us:</u></h3>
-                        <p>Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
-                            With Bambook you can swap the books you no longer need with books you would like to read.</p>
-                        <p>After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.</p>
-                        <p>You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
-                            If a match is found, both users will be notified so they can arrange the book swap details.</p>
-                        <p>
-                            Sounds interesting? Log in or join us today!
-                            <div class="center-align"><br>
-                                <div class="text-center">
-                                    <button id="login" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" type="button" name="login ">
-                                        Login
-                                    </button> 
-                                    <button id="register" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="button" name="register">
-                                            Register
-                                    </button>
-                                </div>
-                            </div>
-                        </p>
-                        <br>
+                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline text-center">Welcome to Bambook!</h3>
                     </div>
                     <!-- Image card 1 -->
                     <div class="card text-center" style="width: 22em;">
                         <img class="card-img-top" src='<?php echo base_url('images/about_us_images/1.png');?>' alt="upload a book">
-
-
                         <div class="card-body">
-                            <p class="card-text">Upload a book you no longer desire, you can either manually enter the information or scan the book's ISBN number.</p>
-                            <a href="#" class="btn btn-primary">List a book</a>
+                            <p class="card-text">
+                                Bambook is an eco-friendly easy to use platform that allows you to swap books for free with our growing community.
+                                With Bambook you can swap the books you no longer need with books you would like to read.<br>
+                                After you register to Bambook, you can list the books you own and wish to exchange, and search for books you would like to read.<br>
+                                You can request a book swap from the available books that Bambook's users have uploaded and suggest the book you would like to trade.
+                                If a match is found, both users will be notified so they can arrange the book swap details.<br>
+                            </p>
+                            <!-- <a href="#" class="btn btn-primary">List a book</a> -->
                         </div>
                     </div>
                     <br><br><br>
                     <!-- Image card 2 -->
                     <div class="card text-center" style="width: 22em;">
-                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/2.png');?>' alt="search a book">
+                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/2.png');?>' alt="upload a book">
+
+
                         <div class="card-body">
-                            <p class="card-text">Search a book from the available Bambook's library which is growing every single day!</p>
-                            <a href="#" class="btn btn-primary">Search a book</a>
+                            <p class="card-text">Upload a book you no longer desire, you can either manually enter the information or scan the book's ISBN number.</p>
+                            <!-- <a href="#" class="btn btn-primary">List a book</a> -->
                         </div>
                     </div>
                     <br><br><br>
                     <!-- Image card 3 -->
                     <div class="card text-center" style="width: 22em;">
-                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/3.png');?>' alt="request or get a book swap">
+                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/3.png');?>' alt="search a book">
                         <div class="card-body">
-                            <p class="card-text">Found a book that interest you? Request a swap from the person who listed the book and offer a book instead.<br>
-                            <br>Received a request? Accept or decline the swap, if both sides agree (yay!), arrange the book swap details!</p>
-                            <a href="#" class="btn btn-primary">Request or get a swap</a>
+                            <p class="card-text">Search a book from the available Bambook's library which is growing every single day!</p>
+                            <!-- <a href="#" class="btn btn-primary">Search a book</a> -->
                         </div>
                     </div>
                     <br><br><br>
                     <!-- Image card 4 -->
                     <div class="card text-center" style="width: 22em;">
-                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/4.png');?>' alt="read a book">
+                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/4.png');?>' alt="request or get a book swap">
                         <div class="card-body">
-                            <p class="card-text">Finished your Bambook Swap? Start reading your new book! Happy reading 😊</p>
-                            <a href="#" class="btn btn-primary">Read</a>
+                            <p class="card-text">Found a book that interest you? Request a swap from the person who listed the book and offer a book instead.<br>
+                            <br>Received a request? Accept or decline the swap, if both sides agree (yay!), arrange the book swap details!</p>
+                            <!-- <a href="#" class="btn btn-primary">Request or get a swap</a> -->
                         </div>
                     </div>
                     <br><br><br>
+                    <!-- Image card 5 -->
+                    <div class="card text-center" style="width: 22em;">
+                        <img class="card-img-top" src='<?php echo base_url('images/about_us_images/5.png');?>' alt="read a book">
+                        <div class="card-body">
+                            <p class="card-text">Finished your Bambook Swap? Start reading your new book! Happy reading 😊</p>
+                            <!-- <a href="#" class="btn btn-primary">Read</a> -->
+                        </div>
+                    </div>
+                    <br><br><br>
+                    <div class="center-align"><br>
+                        <p>Sounds interesting? Log in or join us today!<p><br>
+                        <div class="text-center">
+                            <button id="login" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" type="button" name="login ">
+                                Login
+                            </button> 
+                            <button id="register" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="button" name="register">
+                                Register
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
