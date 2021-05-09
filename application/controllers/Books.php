1820d3081e596e646b46827772c131c7058e8f2c
Assaf Zick <zickmusic@gmail.com>
2021-05-09 17:31:13 +0300

    uploading an image

--- application/controllers/Books.php
+++ application/controllers/Books.php
@@ -6,7 +6,8 @@ class Books extends CI_Controller{
         $this->load->helper('url');
         $this->load->helper('form');
         $this->load->library('form_validation');
-        $this->load->library('session');     
+        $this->load->library('session');    
+        $this->load->helper(array('form', 'url')); 
     }
 
     // Function to load the available books from the index page
@@ -222,5 +223,34 @@ class Books extends CI_Controller{
         $this->load->view('B_Views/test_page2',$data);
     
     }    
-        
+    
+    public function do_upload()
+        {
+                $config['upload_path']          = './uploads/';
+                $config['allowed_types']        = 'gif|jpg|png';
+                $config['max_size']             = 1000;
+                $config['max_width']            = 1980;
+                $config['max_height']           = 1020;
+
+                $this->load->library('upload', $config);
+
+                if ( ! $this->upload->do_upload('userfile'))
+                {
+                        $error = array('error' => $this->upload->display_errors());
+
+                        $this->load->view('upload_form', $error);
+                }
+                else
+                {
+                        $data = array('upload_data' => $this->upload->data());
+
+                        $this->load->view('upload_success', $data);
+                }
+        }
+
+    public function to_upload(){
+        $this->load->view('B_Views/upload_form');
+
+    }
+
 }
