c2556e4cac04e3fbdf573041d8386687766c480a
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-09 14:47:11 +0300

    changes

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -87,6 +87,7 @@
                         </div>
                     </div>
                 </div>
+                <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
             </div>
         </main>
 
@@ -208,13 +209,26 @@
                                             document.getElementById('book_language').value = 'Russian';
                                         }
                                         
-                                        $(window).scroll(function() {
-                                            if ($(this).scrollTop() == ($(document).height() - $(window).height())) {
-                                        //        console.log($(this).scrollTop());
-                                        //        console.log($(document).height());
-                                        //        console.log($(window).height());
-                                                alert('Your book was added to the form successfully!');
-                                            }
-                                        });
+                                        
+                                   }
+
+                                   //Get the button
+                                    var mybutton = document.getElementById("myBtn");
+
+                                    // When the user scrolls down 20px from the top of the document, show the button
+                                    window.onscroll = function() {scrollFunction()};
+
+                                    function scrollFunction() {
+                                        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                                            mybutton.style.display = "block";
+                                        } else {
+                                            mybutton.style.display = "none";
+                                        }
+                                    }
+
+                                    // When the user clicks on the button, scroll to the top of the document
+                                    function topFunction() {
+                                        document.body.scrollTop = 0;
+                                        document.documentElement.scrollTop = 0;
                                     }
                                 </script>
