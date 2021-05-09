9427eb82855ddb67a1d577ba9c4addc5329ac13d
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-09 13:20:26 +0300

    changes form

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -184,6 +184,22 @@
                                         document.getElementById('book_author').value = $('.thumbs')[i].childNodes[5].wholeText; // to fix the right side
                                         document.getElementById('book_language').value = $('.thumbs')[i].childNodes[11].wholeText; // to fix the right side
                                         document.getElementById('b_isbn').value = $('.thumbs')[i].childNodes[14].wholeText; // to fix the right side
+                                    
+                                        if ($('.thumbs')[i].childNodes[11].wholeText == 'en' || ($('.thumbs')[i].childNodes[11].wholeText == 'en-GB') {
+                                            document.getElementById('book_language').value = 'English';
+                                        }
+                                        if ($('.thumbs')[i].childNodes[11].wholeText == 'iw') {
+                                            document.getElementById('book_language').value = 'Hebrew';
+                                        }
+                                        if ($('.thumbs')[i].childNodes[11].wholeText == 'ar') {
+                                            document.getElementById('book_language').value = 'Arabic';
+                                        }
+                                        if ($('.thumbs')[i].childNodes[11].wholeText == 'es') {
+                                            document.getElementById('book_language').value = 'Spanish';
+                                        }
+                                        if ($('.thumbs')[i].childNodes[11].wholeText == 'ru') {
+                                            document.getElementById('book_language').value = 'Russian';
+                                        }
                                     }
 
                                     // function autoFill() {
