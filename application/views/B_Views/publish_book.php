9fee135ecfe1f2c00d015c53b8d0a8fd4522422a
aridan12 <68404161+aridan12@users.noreply.github.com>
2021-05-09 13:24:28 +0300

    changes form

--- application/views/B_Views/publish_book.php
+++ application/views/B_Views/publish_book.php
@@ -104,7 +104,7 @@
                                         searchQuery = searchQuery.split(" ").join("+");
 
                                         if (!searchQuery) {
-                                            searchQuery = "paleo";
+                                            searchQuery = "Harry Potter"; // defauly query
                                         }
 
                                         $.ajax({
