1beb766756d7a1f0bc22c8496c13fbaa4d6b045c
Assaf Zick <zickmusic@gmail.com>
2021-05-10 19:13:40 +0300

    change upload path

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -15,7 +15,7 @@ class Upload extends CI_Controller {
 
         public function do_upload()
         {
-                $config['upload_path']          = './uploads/';
+                $config['upload_path']          = '../uploads/';
                 $config['allowed_types']        = 'gif|jpg|png|jpeg';
                 $config['max_size']             = 100;
                 $config['max_width']            = 1024;
