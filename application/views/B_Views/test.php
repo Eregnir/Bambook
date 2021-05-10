70eb20b4588735b065b97169f5e7097a6dd4b8a6
Assaf Zick <zickmusic@gmail.com>
2021-05-10 13:55:31 +0300

    again

--- application/views/B_Views/test.php
+++ application/views/B_Views/test.php
@@ -7,24 +7,8 @@
 </head>
 <body>
     <div>
-       
-    
-    <?php
-        echo form_open_multipart('image_control/index');
-        echo form_input('a_name','Animal Name');
-        echo form_input('a_info','Animal Information');
-        echo form_upload('userfile');
-        echo form_submit('upload','Upload');
-        echo form_close();
-    ?>
-
-        <?php foreach ($images as $image):?>
-        <h1><?php echo $image['a_name'];?></h1>
-        <h1><?php echo $image['a_details'];?></h1>
-        <?php echo '<img src ="'. base_url().'images1/'.$image['a_photo'].'" >';
-        endforeach; ?>
-
-
+        <?php echo $title ?> <br>
+        <span class="card-img-top"><?php echo '<img class="card-img-top" alt="Book Image"src="data:image/jpeg;base64,'.base64_encode( $img).'"/>';?></span>
     </div>
 </body>
 </html>
