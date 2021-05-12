809533d9eeb75f3493c5554fcd66bee30b79733e
Assaf Zick <zickmusic@gmail.com>
2021-05-12 16:22:57 +0300

    lets do it

--- application/views/B_Views/available_books.php
+++ application/views/B_Views/available_books.php
@@ -18,7 +18,7 @@
                         <?php foreach ($books as $book):
                         //Open a form that will send the book UID to the controller in order to show it's full details.
                                 $attributes = array('id' => $book->UID, 'name' =>$book->UID);?>
-                                <tr id="<?php echo 'book_'.$book->UID?>" class="table-row" onclick="submitit(<?php echo $book->UID?>);">
+                                <tr id="<?php echo 'book_'.$book->UID?>" class="table-row" onclick="submitit(<?php echo 'submit_'.$book->UID;?>)">
                                     <td class="w-25">
                                         <!--Open the form -->
                                     <?php echo form_open('Books/single_book', $attributes); ?>
@@ -47,9 +47,11 @@
                                 });
                             });
                             
-                        function submitit(id){
-                            document.getElementById('submit_'.id).click();
-                        }
+                        function submitit(id)
+                            {
+                                document.getElementById(id).click();
+                                };
+
                     </script>
                 <!-- Books table end -->
             </div>
