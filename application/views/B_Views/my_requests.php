62cff9968a8905a0c2d417213010b39c6eb2bdbf
Assaf Zick <zickmusic@gmail.com>
2021-05-17 00:42:38 +0300

    now really restart

--- application/views/B_Views/my_requests.php
+++ application/views/B_Views/my_requests.php
@@ -1,88 +1,81 @@
         <main class="mdl-layout__content">
             <div class="mdl-grid portfolio-max-width">
-
-            
-                <!----------------------- Received requests ------------------------------->
-                <div class="Received_Requests">
-                    <span class="material-icons">mark_email_unread</span><h2 class="pad5">Received Requests</h2>
-
+                <span class="material-icons">mark_email_unread</span><h2 class="pad5">Received Requests</h2>
+                <!-- Received requests -->
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for the book or author name..." title="Type in a name">
+                <br>
+                <table class="table table-image" id="myTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">Requested Book</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Sent By</th>
+                        </tr>
+                    </thead>
                     
-                    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for the book or author name..." title="Type in a name">
-                    <br>
-                    <table class="table table-image" id="myTable">
-                        <thead>
-                            <tr>
-                                <th scope="col">Requested Book</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Sent By</th>
+                    <tbody>
+                    <?php foreach ($requests_in as $req):
+                    
+                    //Open a form that will send the book UID to the controller in order to show it's full details.
+                            $attributes = array('id' => $req->UID, 'name' =>$req->UID);?>
+                            <tr id="<?php echo '_'.$req->swap_UID?>" class="table-row" onclick="submitit(this.id)">
+                                <td class="w-25">
+                                <!-- Open a form that will send the swap UID to the controller in order to select it and show the full request details and options. -->                                     
+                                <?php echo form_open('Books/zoom_swap', $attributes); ?>
+                                <!-- need to change some info here... b_UID etc and complete the correct form for the swap ID -->
+                                    <input type="hidden" value="<?php echo $req->swap_UID;?>" name="swap_UID" id="<?php echo $req->swap_UID?>"> 
+                                    <span class="img-fluid"> <img src="<?php echo base_url('images/user_uploads/'.$req->img_title);?>" alt="Book Photo" style="max-height:200px; max-width: 100%;"> <br></span>
+
+                                    <button id="<?php echo 'submit_'.$req->swap_UID;?>" class="mdl-button mdl-js-button mdl-button--icon hidden" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
+                                <?php echo form_close(); ?>
+                                </td>
+                                <td> <?php echo $req->title ?></td>
+                                <td> <?php echo $req->swap_status?></td>
+                                <td> <?php echo $req->sent_by_username?></td>
                             </tr>
-                        </thead>
-                        
-                        <tbody>
-                        <?php foreach ($requests_in as $req):
-                        //Open a form that will send the book UID to the controller in order to show it's full details.
-                                $attributes = array('id' => $req->UID, 'name' =>$req->UID);?>
-                                <tr id="<?php echo '_'.$req->swap_UID?>" class="table-row" onclick="submitit(this.id)">
-                                    <td class="w-25">
-                                    <!-- Open a form that will send the swap UID to the controller in order to select it and show the full request details and options. -->                                     
-                                    <?php echo form_open('Books/zoom_swap', $attributes); ?>
-                                    <!-- need to change some info here... b_UID etc and complete the correct form for the swap ID -->
-                                        <input type="hidden" value="<?php echo $req->swap_UID;?>" name="swap_UID" id="<?php echo $req->swap_UID?>"> 
-                                        <span class="img-fluid"> <img src="<?php echo base_url('images/user_uploads/'.$req->img_title);?>" alt="Book Photo" style="max-height:200px; max-width: 100%;"> <br></span>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table> <br><br><br>
 
-                                        <button id="<?php echo 'submit_'.$req->swap_UID;?>" class="mdl-button mdl-js-button mdl-button--icon hidden" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
-                                    <?php echo form_close(); ?>
-                                    </td>
-                                    <td> <?php echo $req->title ?></td>
-                                    <td> <?php echo $req->swap_status?></td>
-                                    <td> <?php echo $req->sent_by_username?></td>
-                                </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                        </table> <br><br><br>
-                
-                </div>
-                <!----------------------- Sent requests ------------------------------->
 
-                <div class="sent_requests">
-                    <span class="material-icons">send</span>&nbsp;<h2>Sent Requests</h2>
-                    <!-- Sent requests -->
-                    <input type="text" id="myInput2" onkeyup="myFunction()" placeholder="Search for the book or author name..." title="Type in a name">
-                    <br>
-                    <table class="table table-image" id="myTable2">
-                        <thead>
-                            <tr>
-                                <th scope="col">Requested Book</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Sent To</th>
-                                <th scope="col">Status</th>
+                <span class="material-icons">send</span>&nbsp;<h2>Sent Requests</h2>
+                <!-- Sent requests -->
+                <input type="text" id="myInput2" onkeyup="myFunction()" placeholder="Search for the book or author name..." title="Type in a name">
+                <br>
+                <table class="table table-image" id="myTable2">
+                    <thead>
+                        <tr>
+                            <th scope="col">Requested Book</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Sent To</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+
+                    <?php foreach ($requests_out as $req):
+                            $attributes = array('id' => $req->UID, 'name' =>$req->UID);?>
+                            <tr id="<?php echo '_'.$req->swap_UID?>" class="table-row" onclick="submitit2(this.id)">
+                                <td class="w-25">
+                                <!-- Open a form that will send the swap UID to the controller in order to select it and show the full request details and options. -->                                     
+                                <?php echo form_open('Books/zoom_swap_out', $attributes); ?>
+                                <!-- need to change some info here... b_UID etc and complete the correct form for the swap ID -->
+                                    <input type="hidden" value="<?php echo $req->swap_UID;?>" name="swap_UID" id="swap_<?php echo $req->swap_UID?>">                                     
+                                    <span class="img-fluid"> <img src="<?php echo base_url('images/user_uploads/'.$req->img_title);?>" alt="Book Photo" style="max-height:200px; max-width: 100%;"> <br></span>
+
+                                    <button id="<?php echo 'submit2_'.$req->swap_UID;?>" class="mdl-button mdl-js-button mdl-button--icon hidden" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
+                                <?php echo form_close(); ?>
+                                </td>
+                                <td> <?php echo $req->title ?></td>
+                                <td> <?php echo $req->sent_to_username ?></td>
+                                <td> <?php echo $req->swap_status ?></td>
                             </tr>
-                        </thead>
-                        
-                        <tbody>
-
-                        <?php foreach ($requests_out as $req):
-                                $attributes = array('id' => $req->UID, 'name' =>$req->UID);?>
-                                <tr id="<?php echo '_'.$req->swap_UID?>" class="table-row" onclick="submitit2(this.id)">
-                                    <td class="w-25">
-                                    <!-- Open a form that will send the swap UID to the controller in order to select it and show the full request details and options. -->                                     
-                                    <?php echo form_open('Books/zoom_swap_out', $attributes); ?>
-                                    <!-- need to change some info here... b_UID etc and complete the correct form for the swap ID -->
-                                        <input type="hidden" value="<?php echo $req->swap_UID;?>" name="swap_UID" id="swap_<?php echo $req->swap_UID?>">                                     
-                                        <span class="img-fluid"> <img src="<?php echo base_url('images/user_uploads/'.$req->img_title);?>" alt="Book Photo" style="max-height:200px; max-width: 100%;"> <br></span>
-
-                                        <button id="<?php echo 'submit2_'.$req->swap_UID;?>" class="mdl-button mdl-js-button mdl-button--icon hidden" type="submit" name="submit "><i class="material-icons">open_in_new</i></button>
-                                    <?php echo form_close(); ?>
-                                    </td>
-                                    <td> <?php echo $req->title ?></td>
-                                    <td> <?php echo $req->sent_to_username ?></td>
-                                    <td> <?php echo $req->swap_status ?></td>
-                                </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>    
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+                    
                 <script>
                     $(document).ready(function(){
                         $("#myInput").on("keyup", function() {
