232abec2ecc366567751742f4fdb3a7572f96d79
Assaf Zick <zickmusic@gmail.com>
2021-05-16 19:28:00 +0300

    test

--- application/models/Intro_model.php
+++ application/models/Intro_model.php
@@ -13,8 +13,8 @@ class intro_model extends CI_Model {
     
     public function save_register($data){
         $this->db->db_debug = FALSE; 
-        $email = $this->db->query('SELECT email FROM users WHERE email=  "'.$data['email'].'" ');
-        $username = $this->db->query('SELECT email FROM users WHERE email=  "'.$data['email'].'" ');
+        $email = $this->db->query('SELECT email FROM users WHERE email=  "'.$data['email'].'" LIMIT 1 ');
+        $username = $this->db->query('SELECT email FROM users WHERE email=  "'.$data['email'].'" LIMIT 1 ');
         if (isset($email)){
             $err = 'This email is already in use. Try again!';
             return $email;
