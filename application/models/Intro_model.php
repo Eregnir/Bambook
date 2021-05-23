f3df0ae2a960782159d528ecb32cf96f0c03b966
Assaf Zick <zickmusic@gmail.com>
2021-05-23 12:59:14 +0300

    order by ascending

--- application/models/Intro_model.php
+++ application/models/Intro_model.php
@@ -38,7 +38,7 @@ class intro_model extends CI_Model {
 
     //Get Books from the DB in order to present it in the books library. currently limit is 50, we need pagination or something to deal with large mass of books.
     public function get_books($user){
-        $query=$this->db->query('SELECT * FROM `user_books` WHERE `availability`="1" AND NOT `user_username`= "'.$user['username'].'" ORDER BY `date_added` DESC LIMIT 50;');
+        $query=$this->db->query('SELECT * FROM `user_books` WHERE `availability`="1" AND NOT `user_username`= "'.$user['username'].'" ORDER BY `date_added` ASC LIMIT 50;');
         return $query->result();
     }
 
