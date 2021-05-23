ce83c0ce13aeb547352623e6c0997af391bb5409
Assaf Zick <zickmusic@gmail.com>
2021-05-23 13:32:39 +0300

    Book genre filter 9

--- application/views/B_Views/available_books.php
+++ application/views/B_Views/available_books.php
@@ -107,7 +107,7 @@
                             });
 
                         $(document).ready(function(){
-                            $("#book_genre").on("keyup", function() {
+                            $("#book_genre").on("select", function() {
                                 var value = $(this).val().toLowerCase();
                                 $("#myTable tr").filter(function() {
                                     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
