a38abeb6213a26b2ef8b8402db6ea6cb428b597b
Assaf Zick <zickmusic@gmail.com>
2021-05-26 15:43:52 +0300

    cancel page 2

--- application/controllers/Books.php
+++ application/controllers/Books.php
@@ -123,7 +123,7 @@ class Books extends CI_Controller{
         //check if this is incoming or outgoing request. if true it is an outgoing req.
         $out=$this->books_model->check_inout($data['swap_UID'],$user['username']);
         //if the request was initiated by this user, access the outgoing request view.
-        if (isset($out['swap_status'])){
+        if ($out){
             $data['out'] = $out;
             $this->load->view('B_Views/test_page',$data);
             // $this->zoom_swap_out($data['swap_UID']);
