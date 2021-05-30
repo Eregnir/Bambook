f916c1261b16456f8ce39603a9afd857ad02b673
Assaf Zick <zickmusic@gmail.com>
2021-05-30 12:01:33 +0300

    aligning avatars

--- application/views/B_Views/avatars.php
+++ application/views/B_Views/avatars.php
@@ -4,22 +4,28 @@
                 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
                    
                     <div class="mdl-grid portfolio-copy">
-                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline"><u>Select Avatar:</u></h3>
-                        <?php foreach ($avatars as $avatar):
-                        //Open a form that will send the avatar UID to the controller in order to select it and change the avatar.
-                            $attributes = array('id' => $avatar->avatar_UID);
-                            echo form_open('Users/update_avatar', $attributes); ?>
-                            <input type="hidden" value="<?php echo $avatar->avatar_UID;?>" name="avatar_UID" id="<?php echo 'avatar_'.$avatar->avatar_UID?>"> 
-                        <center><div id="<?php echo $avatar->avatar_UID?>" class="mdl-cell mdl-cell--8-col mdl-card__supporting-text no-padding">
-                            <?php echo '<img style="max-height:150px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $avatar->img).'"/>'; ?>
-                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" type="submit" name="submit ">
-                                    Select
-                                </button>
-                        </div> </center>
-                        <hr style="width:90%;">
-                        <?php echo form_close(); ?>
-                        <?php endforeach; ?>
-                </div>
+                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline text-center"><u>Select Avatar:</u></h3>
+
+                        
+                            <?php foreach ($avatars as $avatar):
+                            //Open a form that will send the avatar UID to the controller in order to select it and change the avatar.
+                                    $attributes = array('id' => $avatar->avatar_UID);?>
+                                    <div style = "margin:auto;">
+                                    <?php echo form_open('Users/update_avatar', $attributes); ?>
+                                    <input type="hidden" value="<?php echo $avatar->avatar_UID;?>" name="avatar_UID" id="<?php echo 'avatar_'.$avatar->avatar_UID?>"> 
+                                    <center><div id="<?php echo $avatar->avatar_UID?>" class="mdl-cell mdl-cell--8-col mdl-card__supporting-text no-padding">
+                                    <?php echo '<img style="max-height:150px; max-width: 100%;" src="data:image/jpeg;base64,'.base64_encode( $avatar->img).'"/>'; ?>
+                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" type="submit" name="submit ">
+                                            Select
+                                        </button>
+                                </div> </center>
+                                <hr style="width:90%;">
+                                <?php echo form_close(); ?>
+      
+                            <?php endforeach; ?>
+                        </div>
+
+                    </div>
             </div>
 
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
